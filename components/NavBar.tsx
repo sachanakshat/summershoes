842191b616d90e72992646d4e13677a7afc23c13
sachanakshat <sachanakshat@gmail.com>
2025-01-09 17:28:55 +0530

    Fixed images and build errors

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import AnimatedButton from "./AnimatedButton";
 import Link from "next/link";
-import Image from "next/image";
 
 const NavBar: React.FC = () => {
     const navigationLinks = [
